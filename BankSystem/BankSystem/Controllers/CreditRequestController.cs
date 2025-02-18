087190483f664cadd430dd0ad5af3c7a1374a9b6
ramm4stein <tsetsy.nedelchev@gmail.com>
2025-02-18 20:51:47 +0200

    123asd

---
+++ BankSystem/BankSystem/Controllers/CreditRequestController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using BankSystem.Models;
+using System.Threading.Tasks;
+
+[Authorize]
+public class CreditRequestController : Controller
+{
+    private readonly UserManager<User> _userManager;
+    private readonly ProjectDbContext _context;
+
+    public CreditRequestController(UserManager<User> userManager, ProjectDbContext context)
+    {
+        _userManager = userManager;
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreditRequest model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            model.UserId = user.Id;
+            model.Status = "Pending";
+            model.RequestDate = DateTime.Now;
+
+            _context.CreditRequests.Add(model);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+        return View(model);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> MyRequests()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        var requests = _context.CreditRequests.Where(r => r.UserId == user.Id).ToList();
+        return View(requests);
+    }
+}
